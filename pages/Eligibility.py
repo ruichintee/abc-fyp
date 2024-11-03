6e1cfdd0dad5cca6ad05cca25e4621e2f4c79829
ruichintee <48561311+ruichintee@users.noreply.github.com>
2024-11-02 23:00:51 -0700

    Update Eligibility.py

--- pages/Eligibility.py
+++ pages/Eligibility.py
@@ -98,7 +98,7 @@ def main():
 
 
             results = rag_chain.invoke(input =  "The user is {} years old, and weighs {} kg. He is in good health, and has travelled recently to the following places: {}."
-            "He also has the following medical history: {}. Determine if the user is eligible to donate blood.".format(age,weight,travel_history,medical_history)
+            "He also has the following medical history: {}. Determine if the user is eligible to donate blood.".format(age,weight,travel_history,medical_conditions)
                                       )
             st.write(results["answer"])
     
