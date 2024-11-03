1618f41eb31870c208aea9eeea398fc2d1ab6679
ruichintee <48561311+ruichintee@users.noreply.github.com>
2024-11-02 23:36:02 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("Welcome to the Singapore Blood Donation App")
-st.write("Learn more about blood donation and find out how you can contribute.")
+st.set_page_config(
+   page_title="Blood Donation Eligibility Checker",
+   page_icon="🩸",
+   layout="wide",
+   initial_sidebar_state="expanded",
+)
+
+st.title("Welcome to the Blood Donation Eligibility Checker")
+st.write("Learn more about your blood donation eligibility and the nearest blood bank")
 
 st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
