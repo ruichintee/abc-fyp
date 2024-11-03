90b81e282c2f9d0186ee70c25946652d602d86f1
ruichintee <48561311+ruichintee@users.noreply.github.com>
2024-11-03 00:24:35 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,26 +4,6 @@ import streamlit as st
 import hmac
 
 
-def check_password():
-    """Returns `True` if the user had the correct password."""
-
-    def password_entered():
-        if st.session_state["password"] == st.secrets["password"]:
-            st.session_state["password_correct"] = True
-            del st.session_state["password"]
-        else:
-            st.session_state["password_correct"] = False
-
-if "password_correct" not in st.session_state:
-    st.text_input("Enter the password", type="password", on_change=password_entered, key="password")
-    return False
-elif not st.session_state["password_correct"]:
-    st.text_input("Enter the password", type="password", on_change=password_entered, key="password")
-    st.error("Password is incorrect")
-    return False
-else:
-    return True
-
 def main():
     st.set_page_config(
         page_title="Blood Donation Eligibility Checker",
@@ -36,6 +16,34 @@ def main():
     st.write("Learn more about your blood donation eligibility and the nearest blood bank")
        
     st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
-    
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 if __name__ == "__main__":
      main()
