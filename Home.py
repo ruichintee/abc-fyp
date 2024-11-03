7fb33c465de3cb4afb8cbfad57ceee5fcef47609
ruichintee <48561311+ruichintee@users.noreply.github.com>
2024-11-02 19:49:37 -0700

    Update Home.py
    Amended by removing buttons and added navigation

--- Home.py
+++ Home.py
@@ -3,19 +3,12 @@ import streamlit as st
 def main():
     st.title("Welcome to the Singapore Blood Donation App")
     st.write("Learn more about blood donation and find out how you can contribute.")
-    
-    # Buttons to navigate to other pages
-    st.write("Navigate to:")
-    if st.button("Am I Eligible"):
-        st.session_state.page = Eligibility.py
-    if st.button("Where to Donate"):
-        st.session_state.page = DonationLocation.py
-    if st.button("About Us"):
-        st.session_state.page = AboutUs.py
-    if st.button("Methodology"):
-        st.session_state.page = Methodology.py
 
 
     st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
+
+    pg = st.navigation([st.Page("Eligibility.py"), st.Page("DonationLocation.py"), st.Page("AboutUs.py"), st.Page("Methodology.py")])
+    pg.run()
+
 if __name__ == "__main__":
     main()
