527d82031399d035b2a5c3f26eb914b6188a4a80
ruichintee <48561311+ruichintee@users.noreply.github.com>
2024-11-02 20:14:26 -0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-def main():
-    st.title("Welcome to the Singapore Blood Donation App")
-    st.write("Learn more about blood donation and find out how you can contribute.")
+st.title("Welcome to the Singapore Blood Donation App")
+st.write("Learn more about blood donation and find out how you can contribute.")
 
-
-    st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
-
-if __name__ == "__main__":
-    main()
+st.write("IMPORTANT NOTICE: This web application is a prototype developed for educational purposes only. The information provided here is NOT intended for real-world usage and should not be relied upon for making any decisions, especially those related to financial, legal, or healthcare matters. Furthermore, please be aware that the LLM may generate inaccurate or incorrect information. You assume full responsibility for how you use any generated output. Always consult with qualified professionals for accurate and personalized advice.")
